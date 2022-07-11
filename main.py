ff8bcd0dd1f38447a949f36b8f359128142fea31
Thi <tiagoqueirozgoncalves100@gmail.com>
2022-07-11 15:42:37 -0300

    Adicionando funcionalidades ao programa
    Adicionando um filtro de eventos nos campos para coletar o evento de click e copiar automaticamente o texto.
    
    Também estou utilizando um timer para adicionar e controlar o tempo que uma alteração é exibida e remover a alteração através de um método que é executado no disparo de timeout.

--- main.py
+++ main.py
@@ -27,7 +27,8 @@ Usando o Pyinstaller:
 """
 from web_scraping import gerar_pessoa
 from display import *
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt5.QtCore import Qt, QTimer
 
 
 class Pessoa(QMainWindow, Ui_MainWindow):
@@ -37,9 +38,56 @@ class Pessoa(QMainWindow, Ui_MainWindow):
 
         # Carregando os dados na inicialização da janela
         self.carregar_dados()
+
+        # Definindo um timer para resetar modificações
+        self.timer = QTimer(self)
+        self.lista_widgets = list()
+        self.timer.timeout.connect(self.resetar)
+
+        # Criando filtro de eventos para os campos de texto.
+        self._Nome.installEventFilter(self)
+        self._CPF.installEventFilter(self)
+        self._Idade.installEventFilter(self)
+        self._Nascimento.installEventFilter(self)
+        self._Email.installEventFilter(self)
+        self._Telefone.installEventFilter(self)
+        self._Endereco.installEventFilter(self)
+        self._Cidade.installEventFilter(self)
+        self._Estado.installEventFilter(self)
+        self._Cep.installEventFilter(self)
+        self._Altura.installEventFilter(self)
+        self._Peso.installEventFilter(self)
+        self._Sangue.installEventFilter(self)
         
         # Recarregando os dados ao clicar no botão
         self.btnGerarPessoa.clicked.connect(self.carregar_dados)
+
+
+    def eventFilter(self, obj, event):
+        """
+        Este Método sobrescreve o Filtro de eventos da classe QWidget
+        Isso permite que o evento de clique seja capturado junto com o objeto que está sendo filtrado.
+
+        :param obj: Objeto que está sendo filtrado
+        :param event: Evento que está sendo filtrado
+        :return: Os mesmos valores do filtro para o método original do eventFilter
+        """
+        if event.type() == event.MouseButtonPress:  # Validando o tipo de evento
+            obj.setCursor(Qt.ClosedHandCursor)
+            obj.selectAll()
+            obj.copy()
+            obj.setStyleSheet("background-color: green;color: white;border-radius: 10px;border: 2px solid grey;")
+            self.lista_widgets.append(obj)  # Adicionando o objeto a lista de widgets
+            self.timer.start(300)  # Iniciando o timer
+        return super(QWidget, self).eventFilter(obj, event)  # Repassando o Filtro de Evento para a classe original
+
+
+    def resetar(self):
+        for item in self.lista_widgets:
+            item.setCursor(Qt.IBeamCursor)
+            item.setStyleSheet("background-color: white;color: black;border-radius: 10px;border: 2px solid grey;")
+        self.lista_widgets.clear()
+        self.timer.stop()
 
 
     def carregar_dados(self):
